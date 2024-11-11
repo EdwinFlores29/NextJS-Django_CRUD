c8214af974f262c5041ea51040ddaa8ce64383bc
Edwin Oswaldo Flores Gutierrez <46845817+EdwinFlores29@users.noreply.github.com>
2024-11-11 11:19:16 -0600

    Backend listo
    Backend listo

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Tasks(models.Model):
+    objects = None
     title = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     done = models.BooleanField(default=False)
